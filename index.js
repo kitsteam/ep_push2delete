af849258b643453593da16e1f17f0ec1708fc50a
Jannik Streek <jannik.streek@gmail.com>
2024-03-18 18:20:34 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 var eejs = require('ep_etherpad-lite/node/eejs')
-//var $ = require('ep_push2delete/node/cheerio');
-//var $ = require('ep_etherpad-lite/node/cheerio');
-var Pad = require('ep_etherpad-lite/node/db/Pad.ts').Pad
-
-
-/*var pluginSettings = settings.ep_push2delete;
-var deleteButton = pluginSettings.settingsDelete || false;
-pluginSettings.settingsDelete = true;*/
+var Pad = require('ep_etherpad-lite/node/db/Pad').Pad
 
 /*
 * Handle incoming delete requests from clients
@@ -48,90 +41,11 @@ exports.handleMessage = function(hook_name, context, callback){
   }
 }
 
-exports.eejsBlock_editbarMenuRight = function(hook_name, args, cb) {
-  //let hiddenState = "display: none !important;";
-  /*if (settings.ep_push2delete) {
-    if (!settings.ep_push2delete.disabledByDefault && !args.renderContext.req.url.match(/^\/(p\/r\..{16})/)) {
-      hiddenState = "display: inline !important;";
-    }
-  }*/
-  /*let hiddenState = "display: inline !important;";
-  const ejsPath = 'ep_push2delete/templates/delete_button.ejs';
-  args.content = eejs.require(ejsPath, {hidden: hiddenState}) + args.content;*/
-  /*if(!args.renderContext.req.url.match(/^\/(p\/r\..{16})/)) {
-    args.content = eejs.require('ep_push2delete/templates/delete_button.ejs') + args.content;
-  }*/
-  cb();
-};
-
-/*exports.expressCreateServer = (hookName, {app}, cb) => {
-  app.get('/admin/delete', (req, res) => {
-    res.send(eejs.require('ep_push2delete/templates/admin/delete.html', {errors: []}));
-  });
-  return cb();
-};*/
-
-/*exports.eejsBlock_adminMenu = (hookName, context, cb) => {
-  const ul = $('<ul>').html(context.content);
-  const pfx = ul.find('li a').attr('href').match(/^((?:\.\.\/)*)/)[1];
-  ul.append(
-      $('<li>').append(
-          $('<a>')
-              .attr('href', `${pfx}delete`)
-              .attr('data-l10n-id', 'ep_adminpads2_manage-pads')
-              .text('Löschbutton aktivieren')));
-  context.content = ul.html();
-  return cb();
-};*/
-
-exports.eejsBlock_adminMenu = (hookName, context, cb) => {
-  /*const ul = $('<ul>').html(context.content);
-  const pfx = ul.find('li a').attr('href').match(/^((?:\.\.\/)*)/)[1];
-  ul.append(
-      $('<li>').append(
-          $('<a>')
-              .attr('href', `${pfx}delete`)
-              .attr('data-l10n-id', 'ep_adminpads2_manage-pads')
-              .text('Löschbutton aktivieren')));
-  context.content = ul.html();*/
-  return cb();
-};
-
-exports.eejsBlock_mySettings = function(hook_name, args, cb) {
-  /*let checkedState = 'checked';
-  if (settings.ep_push2delete) {
-    if (settings.ep_push2delete.disabledByDefault) {
-      checkedState = '';
-    }
-  }
-  const ejsPath = 'ep_push2delete/templates/delete_entry.ejs';
-  args.content += eejs.require(ejsPath, {checked: checkedState});*/
-    
-  /*let hiddenState = "display: inline !important;";
-  const ejsPath = 'ep_push2delete/templates/delete_button.ejs';
-  args.content += eejs.require(ejsPath, {hidden: hiddenState});*/
-  return cb();
-};
-
-exports.eejsBlock_mySettings.dropdowns = function(hook_name, args, cb) {
-  /*let checkedState = 'checked';
-  if (settings.ep_push2delete) {
-    if (settings.ep_push2delete.disabledByDefault) {
-      checkedState = '';
-    }
-  }
-  const ejsPath = 'ep_push2delete/templates/delete_entry.ejs';
-  args.content += eejs.require(ejsPath, {checked: checkedState});*/
-    
+exports.eejsBlock_mySettings.dropdowns = function(_hook_name, args, cb) {
   let hiddenState = "display: inline !important;";
   const ejsPath = 'ep_push2delete/templates/delete_button1.ejs';
   args.content += eejs.require(ejsPath, {hidden: hiddenState});
   return cb();
-};
-
-exports.eejsBlock_dd_view = (hookName, args, cb) => {
-  /*const li = "<li><a href='#' onClick='$(\"#options-delete\").click();'>Löschbutton aktivieren</a></li>";
-  args.content += li;*/
 };
 
 
